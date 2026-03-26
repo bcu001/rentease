20b7b9db4627d266946fa5032b5a49797134f14f
Bhuwan Upadhyay <bhuwan.upadhyay.work@gmail.com>
2026-03-26 09:27:50 +0530

    auth modified

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,56 +1,60 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
 import { User } from "../models/user.model";
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
 import { ENV } from "../config/env";
-import jwt from 'jsonwebtoken'
+import * as jwt from "jsonwebtoken";
 import asyncHandler from "../helpers/asyncHandler";
 import { AppError } from "../helpers/AppError";
 import { normalizeUser } from "../helpers/normalizeUser";
 
-export const logIn = asyncHandler(async(req:Request, res:Response)=>{
-        const {email, password} = req.body;
+export const logIn = asyncHandler(async (req: Request, res: Response) => {
+  const { email, password } = req.body;
 
-        const existingUser = await User.findOne({email}).select("+hashedPassword");
+  const existingUser = await User.findOne({ email }).select("+hashedPassword");
 
-        if(!existingUser){
-            throw new AppError("no user exists", 404);
-        }
+  if (!existingUser) {
+    throw new AppError("no user exists", 404);
+  }
 
-        const validatePass = bcrypt.compare(password, existingUser.hashedPassword);
+  const validatePass = bcrypt.compare(password, existingUser.hashedPassword);
 
-        if(!validatePass){
-            throw new AppError("inccorect password",401);
-        }
+  if (!validatePass) {
+    throw new AppError("inccorect password", 401);
+  }
 
-        const token = jwt.sign({userId:existingUser._id}, ENV.JWT_SECRET , {expiresIn:ENV.JWT_EXPIRE_IN as jwt.SignOptions["expiresIn"] });
+  const token = jwt.sign({ userId: existingUser._id }, ENV.JWT_SECRET, {
+    expiresIn: ENV.JWT_EXPIRE_IN as jwt.SignOptions["expiresIn"],
+  });
 
-        return res.json({
-            success:true,
-            message:"user log in successfully",
-            data:{
-                token,
-                user: normalizeUser(existingUser)
-            }
-        })
-} )
+  return res.json({
+    success: true,
+    message: "user log in successfully",
+    data: {
+      token,
+      user: normalizeUser(existingUser),
+    },
+  });
+});
 
-export const register = asyncHandler(async(req:Request, res:Response)=>{
-    const {name, email, password } = req.body;
+export const register = asyncHandler(async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
 
-    const existingUser = await User.findOne({email});
-    if(existingUser) throw new AppError("this email is already registered");
+  const existingUser = await User.findOne({ email });
+  if (existingUser) throw new AppError("this email is already registered");
 
-    const salt = await bcrypt.genSalt();
+  const salt = await bcrypt.genSalt();
 
-    const hashedPassword = await bcrypt.hash(password, salt);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-    const newUser = await User.create({name , email, hashedPassword});
+  const newUser = await User.create({ name, email, hashedPassword });
 
-   return res.status(201).json({
-    success:true,
-    message:"user as been registered",
-    data:{
-        user:normalizeUser(newUser),
-    }
-   })
-})
+
+
+  return res.status(201).json({
+    success: true,
+    message: "user as been registered",
+    data: {
+      user: normalizeUser(newUser),
+    },
+  });
+});
---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from "express";
+import * as jwt from "jsonwebtoken";
+import { ENV } from "../config/env";
+import { AppError } from "../helpers/AppError";
+import { User } from "../models/user.model";
+import { sendError } from "../helpers/response";
+
+type JwtPlayload = {
+  userId: string;
+};
+
+const authorize = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    Object.assign(req, { context: {} });
+    let token: string;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer ")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    } else {
+      throw new AppError("no token given");
+    }
+
+    const decoded = jwt.verify(token, ENV.JWT_SECRET) as JwtPlayload;
+
+    const user = await User.findById(decoded.userId);
+
+    if (!user) throw new AppError("Unauthorized", 401);
+
+    Object.assign(req, {
+      context: {
+        user,
+        token,
+      },
+    });
+    next();
+  } catch (error: unknown) {
+    return sendError(res, error, 401, "Unauthorized");
+  }
+};
